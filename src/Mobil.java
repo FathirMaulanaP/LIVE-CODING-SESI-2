50a1b110011c19f71f1ae9b6438e2f8cae7f1d56
Fathir Maulana P <fathirmaulanaputra83@gmail.com>
2026-04-09 00:01:41 +0700

    update tugas sesi 2

--- src/Mobil.java
+++ src/Mobil.java
@@ -9,26 +9,31 @@ public class Mobil {
         this.kecepatan = 0;
         this.mesinMenyala = false;
     }
+
     public void Nyalakan() {
-        this.kecepatan = 2;
+        this.kecepatan = 5; // diubah jadi 5 km/jam
         this.mesinMenyala = true;
-        System.out.println(merk + " : mesin dinyalakan,kecepatan : " + kecepatan + "km/jam");
+        System.out.println(merk + " : mesin dinyalakan, kecepatan : " + kecepatan + " km/jam");
     }
+
     public void Matikan() {
         this.kecepatan = 0;
         this.mesinMenyala = false;
-        System.out.println(merk + " : mesin dimatikan,kecepatan : " + kecepatan + "km/jam");
+        System.out.println(merk + " : mesin dimatikan, kecepatan : " + kecepatan + " km/jam");
     }
+
     public void gas() {
         if (mesinMenyala) {
             this.kecepatan += 5;
-            System.out.println(merk + " : mesin digas,kecepatan : " + kecepatan + "km/jam");
-        }else{
-            System.out.println(merk + " : mesin direm,kecepatan : " + kecepatan + "km/jam");
+            System.out.println(merk + " : mesin digas, kecepatan : " + kecepatan + " km/jam");
+        } else {
+            System.out.println(merk + " : mesin belum menyala!");
         }
     }
+
     public void rem() {
         this.kecepatan -= 5;
-        System.out.println(merk + " : mesin direm,kecepatan : " + kecepatan + "km/jam");
+        if (kecepatan < 0) kecepatan = 0; // biar tidak minus
+        System.out.println(merk + " : direm, kecepatan : " + kecepatan + " km/jam");
     }
 }
